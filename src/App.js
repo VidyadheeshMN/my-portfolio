b0d8e985bbf202ec5d9bf90917936cc2a8be1fb5
vidyadheesh <vidyadheesh.3@gmail.com>
2022-07-24 16:51:42 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import Home from "./Components/Home";
+import Resume from "./Components/Resume";
+import NavBar from "./Components/Navbar";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import Projects from "./Components/Projects";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <NavBar />
+        <div className='col'>
+          <div className='row mt-5'></div>
+          <div className='row mt-3'></div>
+        </div>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/resume' element={<Resume />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='*' element={<Navigate to='/' />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/Resume.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Container, Row } from "react-bootstrap";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
+export default function Home() {
+  return <div>this is resume page</div>;
+}
